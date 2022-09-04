26d4301fdc2f3b5f5bc323b8cca0046b47babf11
SidharthK2 <sidharthkapoor.100@gmail.com>
2022-09-04 14:45:33 +0530

    add contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, message);
+  };
+  const handleChange = (e) => {
+    if (e.target.name == "name") {
+      setName(e.target.value);
+    } else if (e.target.name == "email") {
+      setEmail(e.target.value);
+    } else {
+      setMessage(e.target.value);
+    }
+  };
+
   return (
-    <div>
-      <h1>Contact Us</h1>
+    <div className="m-4">
+      <h1 className="m-4 text-xl">Contact Us</h1>
+      <form onSubmit={handleSubmit} className="w-full max-w-lg m-2 p-2">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-password"
+            >
+              Name
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="name"
+              name="name"
+              type="text"
+              value={name}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-password"
+            >
+              E-mail
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="email"
+              name="email"
+              type="email"
+              value={email}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-password"
+            >
+              Message
+            </label>
+            <textarea
+              className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+              id="message"
+              name="message"
+              value={message}
+              onChange={handleChange}
+            ></textarea>
+          </div>
+        </div>
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3">
+            <button
+              className="shadow bg-teal-400 hover:bg-teal-500 active:scale-95 text-white font-bold py-2 px-4 rounded"
+              type="submit"
+            >
+              Send
+            </button>
+          </div>
+          <div className="md:w-2/3"></div>
+        </div>
+      </form>
     </div>
   );
 }
